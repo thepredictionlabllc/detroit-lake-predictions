89aa4ade40e3cb7a201d182e9afd75b41de182eb
James Watson <watsonjr@users.noreply.github.com>
2022-05-04 09:10:36 -0700

    Update plot_chla.py

--- scripts/plot_chla.py
+++ scripts/plot_chla.py
@@ -14,52 +14,11 @@ from scipy.interpolate import griddata
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-from mpl_toolkits import axes_grid1
-
-def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
-    """Add a vertical color bar to an image plot."""
-    divider = axes_grid1.make_axes_locatable(im.axes)
-    width = axes_grid1.axes_size.AxesY(im.axes, aspect=1./aspect)
-    pad = axes_grid1.axes_size.Fraction(pad_fraction, width)
-    current_ax = plt.gca()
-    cax = divider.append_axes("right", size=width, pad=pad)
-    plt.sca(current_ax)
-    return im.axes.figure.colorbar(im, cax=cax, **kwargs)
-
-
-### Setup map
-def image_spoof(self, tile): # this function pretends not to be a Python script
-    url = self._image_url(tile) # get the url of the street map API
-    req = Request(url) # start request
-    req.add_header('User-agent','Anaconda 3') # add user agent to request
-    fh = urlopen(req)
-    im_data = io.BytesIO(fh.read()) # get image
-    fh.close() # close url
-    img = Image.open(im_data) # open image with PIL
-    img = img.convert(self.desired_tile_form) # set image format
-    return img, self.tileextent(tile), 'lower' # reformat for cartopy
-
-cimgt.OSM.get_image = image_spoof # reformat web request for street map spoofing
-#osm_img = cimgt.OSM() # spoofed, downloaded street map << Open Street Map
-osm_img = cimgt.QuadtreeTiles() # spoofed, downloaded street map << Sat image
-
-
 
 ################ SATELLITE CHL-A ####################
 #path = "./Data/or_detroit_lake_dashboard/proc_dashboard_data/"
 path = "/tmp/or_detroit_lake_dashboard/proc_dashboard_data/"
 files = sorted(glob.glob(path+"satellite_map/*.csv"))
-
-##! find colorbounds
-#chl_min = [] 
-#chl_min = [] 
-#for i in np.arange(int(len(files)/2),len(files)):
-#	data = pd.read_csv(files[i],parse_dates=["date"])
-#	chl_min = np.append(chl_min,np.percentile(data['Chlorophyll'],5))
-#	chl_max = np.append(chl_max,np.percentile(data['Chlorophyll'],95))
-#	print(i)
-#chl_min = np.percentile(chl_min,5)
-#chl_max = np.percentile(chl_max,95)
 
 #! Latest data
 data = pd.read_csv(files[-1],parse_dates=["date"])
@@ -79,6 +38,7 @@ for i in np.arange(-X,-1):
             (lons, lats), method='nearest')) / 2
 
 
+	
 
 #################### MAKE MAP (average of last week) ################
 #! Data to plot
@@ -86,49 +46,31 @@ x = np.asarray(df.lon)
 y = np.asarray(df.lat)
 z = np.asarray(df.Chlorophyll)
 ID = np.where(z!=0)[0]
-x = x[ID]; y = y[ID]; z = z[ID]
-zl = np.log10(z+1)
+x = x[ID]; y = y[ID];
+z = np.exp(z[ID])
+z = z / z.max()
+z = np.round(z * 100)
 
-fig = plt.figure(figsize=(7,5.5)) # open matplotlib figure
-ax1 = plt.axes(projection=osm_img.crs) # project using coordinate reference system (CRS) of street map
+df = {'x': x,
+        'y': y,
+        'z': z}
+df = pd.DataFrame(df)
 
+import plotly.figure_factory as ff
+import plotly.express as px
+
+px.set_mapbox_access_token(open(".mapbox_token").read())
 date_time = data.date[0].strftime("%m/%d/%Y")
-plt.title("Chlorophyll-a (Sentinel 2a): " + date_time)
+title = "Chlorophyll-a Index (Sentinel 2a): " + date_time
 
-center_pt = [np.mean(data.lat), np.mean(data.lon)] # lat/lon of One World Trade Center in NYC
-zoom = 0.05 # for zooming out of center point
-extent = [center_pt[1]-(zoom*2.0),center_pt[1]+(zoom*2.0), \
-	center_pt[0]-zoom,center_pt[0]+zoom] # adjust to zoom
-ax1.set_extent(extent) # set extents
+fig = ff.create_hexbin_mapbox(
+    data_frame=df, lat="y", lon="x", color="z",
+    nx_hexagon=70, opacity=0.5,
+    range_color=[0,100],
+    labels={"color": "Chl-a Index"},agg_func=np.mean,color_continuous_scale="jet")
 
-scale = np.ceil(-np.sqrt(2)*np.log(np.divide(zoom,350.0))) # empirical solve for scale based on zoom
-scale = (scale<14) and scale or 14 # scale cannot be larger than 19
-ax1.add_image(osm_img, int(scale)) # add OSM with zoom specification
-# NOTE: zoom specifications should be selected based on extent:
-# -- 2     = coarse image, select for worldwide or continental scales
-# -- 4-6   = medium coarseness, select for countries and larger states
-# -- 6-10  = medium fineness, select for smaller states, regions, and cities
-# -- 10-12 = fine image, select for city boundaries and zip codes
-# -- 14+   = extremely fine image, select for roads, blocks, buildings
-
-#! colorlimits (in this specific data)
-#vmin = np.percentile(z,5)
-#vmax = np.percentile(z,95)
-vmin = 0.5
-vmax = 2.25
-
-#! plot points
-im = ax1.scatter(x,y,4,z,marker='s',alpha=.9,vmin=vmin,vmax=vmax,transform=ccrs.PlateCarree(),cmap="jet",edgecolors='none')
-
-#! Colorbar
-cax = fig.add_axes([ax1.get_position().x1+0.01,ax1.get_position().y0,0.02,ax1.get_position().height])
-cbar = plt.colorbar(im,cax=cax)
-ticks = cbar.get_ticks()
-ticks = len(ticks)*[None]
-ticks[0] = "Low"
-ticks[-1] = "High"
-cbar.ax.set_yticklabels(ticks)
-
-
-plt.savefig("./Figs/Fig_chla_week.png",dpi=300,bbox_inches='tight')
+fig.update_layout(title_text=title,title_y=0.92,title_x=0.2)
+#fig.update_layout(mapbox_style='open-street-map')
+fig.update_layout(mapbox_style="satellite")
+fig.write_image("Figs/Fig_chla_week.png",scale=2)
 plt.close("all")
