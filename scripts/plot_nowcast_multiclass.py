e3b2cd729979f70fb3d68c901a48705fa535ef81
A. John Woodill <johnwoodill@gmail.com>
2024-01-15 08:47:46 -0800

    Update plot_nowcast_multiclass.py

--- scripts/plot_nowcast_multiclass.py
+++ scripts/plot_nowcast_multiclass.py
@@ -47,6 +47,57 @@ probs_1 = np.asarray([data.iloc[[-1]]['none_bloom_p'],data.iloc[[-1]]['low_bloom
 			data.iloc[[-1]]['mid_bloom_p'],data.iloc[[-1]]['high_bloom_p']])
 
 
+# Create the figure and axes
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5))
+
+# Plot the first pie chart with legend and no labels
+labels = ('No Detection', 'Algae Present', 'Algal Bloom')
+probs = probs_14.flatten()
+probs = [probs[0], probs[1] + probs[2], probs[3]]
+probs = probs / np.sum(probs)
+sizes = probs * 100
+explode = np.ones(len(sizes)) * 0.03
+colors = plt.cm.Greens(np.linspace(0.15, 0.7, len(probs)))
+ax1.pie(sizes, colors=colors, autopct='%1.1f%%', startangle=95, pctdistance=0.8, explode=explode, labels=None)
+centre_circle1 = plt.Circle((0, 0), 0.30, fc='white')
+ax1.add_artist(centre_circle1)
+ax1.axis('equal')
+ax1.annotate('-14days',
+             xy=(0, 0),  # theta, radius
+             horizontalalignment='center',
+             verticalalignment='center',
+             color='k', zorder=21, fontsize=23)
+ax1.legend(labels, loc="best", fontsize=12)
+
+# Plot the second pie chart with legend and no labels
+labels = ('No Detection', 'Algae Present', 'Algal Bloom')
+probs = probs_1.flatten()
+probs = [probs[0], probs[1] + probs[2], probs[3]]
+probs = probs / np.sum(probs)
+sizes = probs * 100
+explode = np.ones(len(sizes)) * 0.03
+colors = plt.cm.Reds(np.linspace(0.15, 0.7, len(probs)))
+ax2.pie(sizes, colors=colors, autopct='%1.1f%%', startangle=95, pctdistance=0.8, explode=explode, labels=None)
+centre_circle2 = plt.Circle((0, 0), 0.30, fc='white')
+ax2.add_artist(centre_circle2)
+ax2.axis('equal')
+ax2.annotate('+7days',
+             xy=(0, 0),  # theta, radius
+             horizontalalignment='center',
+             verticalalignment='center',
+             color='k', zorder=21, fontsize=23)
+ax2.legend(labels, loc="best", fontsize=12)
+
+plt.tight_layout()
+plt.savefig("./Figs/Fig_prediction.png", dpi=600)
+plt.show()
+
+
+
+
+
+
+# Old figure without legend
 ######################! Plot double pie
 fig, (ax1,ax2) = plt.subplots(1,2,figsize=(13,5))
 
